4034dc8593e138f2464517ec08f83332522751a1
SadikMaxLK <lamexs28@gmail.com>
2019-01-20 10:25:51 +0300

    init

---
+++ VkBot/Controllers/CallbackController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using VkNet.Model;
+
+namespace VkBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CallbackController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Тип события
+            switch (updates.Type)
+            {
+                // Ключ-подтверждение
+                case "confirmation":
+
+                    return new OkObjectResult(_configuration["Config:Confirmation"]);
+
+                // Новое сообщение
+                case "message_new":
+                    // Десериализация
+                    var msg = JsonConvert.DeserializeObject<Message>(updates.Object.ToString());
+                    
+                    break;
+            }
+
+            return new OkObjectResult("ok");
+        }
+    }
+}
---
+++ VkBot/Controllers/Updates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace VkBot.Controllers
+{
+	[Serializable]
+	public class Updates
+	{
+		/// <summary>
+		/// Тип события
+		/// </summary>
+		[JsonProperty("type")]
+		public object Type { get; set; }
+
+		/// <summary>
+		/// Объект, инициировавший событие
+		/// Структура объекта зависит от типа уведомления
+		/// </summary>
+		[JsonProperty("object")]
+		public object Object { get; set; }
+
+		/// <summary>
+		/// ID сообщества, в котором произошло событие
+		/// </summary>
+		[JsonProperty("group_id")]
+		public object GroupId { get; set; }
+
+		/// <summary>
+		/// Секретный ключ. Передается с каждым уведомлением от сервера
+		/// </summary>
+		[JsonProperty("secret")]
+		public object Secret { get; set; }
+	}
+}
